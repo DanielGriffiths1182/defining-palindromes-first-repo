7577d0777d4a71e3e77f37a507198f00180831b1
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-23 15:14:52 -0500

    deleted text file

--- Palendrome.rb
+++ Palendrome.rb
@@ -1,1 +1,14 @@
-puts "hello everyone"
+puts "Give me some (any) text!"
+  u_i = gets.chomp
+
+    def is_palindrome?(u_i)
+      u_i = u_i.gsub(/<\/?[^>]*>/, '')
+      if u_i == u_i.reverse
+        print "Thats a palindrome"
+
+      else
+        print "Thats not a palindrome"
+      end
+    end
+
+puts is_palindrome?(u_i)
